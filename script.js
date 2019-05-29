38d928241dc4056f02cbad5ea14b60da71f4d133
Bread <123@gmail.com>
2019-05-29 17:15:42 +0530

    push

--- script.js
+++ script.js
@@ -166,16 +166,130 @@ console.log(a, b);
 
 //FIBONACCIS SERIES//////////////
 
-var a=0;
-var b=1;
+var a = 0;
+var b = 1;
 var c;
 
-for(i=1; i<=10; i++){
-    c=a+b;
+for (i = 1; i <= 10; i++) {
+    c = a + b;
     console.log(c);
-    a=b;
-    b=c;
+    a = b;
+    b = c;
 }
+
+
+//Array-rev///////////
+
+
+/* var num = [26, 27, 28, 29];
+console.log(num.reverse()); */
+
+/////////////
+
+
+var num = [26, 27, 28, 29];
+var rev = [];
+
+for (i = num.length - 1; i >= 0; i--) {
+    rev.push(num[i]);
+
+}
+console.log(rev);
+
+
+//SUM OF ARRAY////
+
+var num1 = [5, 10, 5, 10]
+    .reduce(add);
+function add(total, num1) {
+    return total + num1;
+}
+
+console.log(num1);
+
+///////////////////
+
+var num = [12, 13, 14, 15];
+var add = [];
+
+for (i = 0; i <= num.length - 1; i++) {
+    add.push(num[i]);
+
+}
+console.log(add, num);
+
+
+//////////////////////////
+
+
+
+var unique = [26, 12, 15, 22, 26, 15, 15, 98];
+var count = {};
+unique.forEach(function (i) { count[i] = (count[i] || 0) + 1; });
+console.log(count);
+
+
+var nu = [1, 2, 3, 1, 3, 4, 4];
+for (i = 0; i <= nu.length; i++) {
+    for (j = i + 1; j <= nu.length; j++) {
+        if (nu[i] === nu[j]) {
+            console.log('Duplicate value of', nu[i]);
+        }
+
+    }
+}
+
+
+var arr = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+var arr1 = [];
+var arr2 = [];
+
+
+for (i = 0; i <= arr.length; i++) {
+    if (a[i] % 2 == 0) {
+        arr2.push(arr[i]);
+    } else {
+        arr1.push(arr[i]);
+    }
+}
+console.log(arr2, arr1);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
